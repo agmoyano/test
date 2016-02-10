d7b8875084060866fbabd2d8acab824783db7175
Agustin Moyano <agustin.moyano@gmail.com>
2016-02-10 16:11:49 -0300

    algo

--- test.js
+++ test.js
@@ -2,7 +2,11 @@ var test = require('tape');
 var request = require('request');
 test(function(t) {
   request('http://localhost:8081/test', function(error, header, body) {
-    t.equals('hola', body);
-    t.end();
+    t.equals(body, 'hola');
+    request('http://localhost:8081/test2', function(error, header, body) {
+      t.equals(body, 'hola');
+      t.end();
+    });
   });
+
 });
