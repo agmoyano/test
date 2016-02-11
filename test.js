b3f125d80cb9cf188f0a9973f8616773c347525d
Agustin Moyano <agustin.moyano@gmail.com>
2016-02-11 15:45:55 -0300

    algo

--- test.js
+++ test.js
@@ -6,12 +6,31 @@ test('/test', function(t) {
   request(app)
     .get('/test')
     .expect(200)
-    .expect('hola1', t.end);
+    .end(function(err, res) {
+      if(err) return t.end(err);
+      t.equal(res.text, 'hola', 'La respuesta debe ser igual a "hola"');
+      t.end();
+    });
 });
 
 test('/test2', function(t) {
   request(app)
     .get('/test2')
     .expect(200)
-    .expect('hola', t.end);
+    .end(function(err, res) {
+      if(err) return t.end(err);
+      t.equal(res.text, 'hola', 'La respuesta debe ser igual a "hola"');
+      t.end();
+    });
+});
+
+test('/test3', function(t) {
+  request(app)
+    .get('/test3')
+    .expect(200)
+    .end(function(err, res) {
+      if(err) return t.end(err);
+      t.equal(res.text, 'hola', 'La respuesta debe ser igual a "hola"');
+      t.end();
+    });
 });
